1ff3276abcb2f23cf7216fc3e0f6be1d8eb19bfc
romainlavabre <romainlavabre98@gmail.com>
2025-02-07 12:48:03 +0100

    [1.0.0] ADD fairfair-cloud/support#147

---
+++ collector/zigbee2mqtt-data/external_converters/TS601-water-meter.js
@@ -0,0 +1,10 @@
+const definition = {
+    zigbeeModel: ['TS0601'],
+    model: 'TS0601',
+    vendor: '_TZE200_vuwtqx0t',
+    description: 'Automatically generated definition',
+    extend: [],
+    meta: {},
+};
+
+module.exports = definition;
