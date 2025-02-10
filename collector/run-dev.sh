0a860600c5330365556e1078c7d88429c71f0336
romainlavabre <romainlavabre98@gmail.com>
2025-02-10 16:29:04 +0100

    [1.0.0] ADD fairfair-cloud/support#147

--- collector/run-dev.sh
+++ collector/run-dev.sh
@@ -4,8 +4,6 @@ sudo mkdir -p "/var/docker-volume/home-automation-transmitter/zigbee2mqtt"
 sudo chown -Rf $USER /var/docker-volume/home-automation-transmitter/zigbee2mqtt
 cp configuration.yaml /var/docker-volume/home-automation-transmitter/zigbee2mqtt/configuration.yaml
 
-#cp configuration.yaml ./zigbee2mqtt-data/configuration.yaml
-
 docker compose up
 
 sudo rm -Rf /var/docker-volume/home-automation-transmitter/zigbee2mqtt
