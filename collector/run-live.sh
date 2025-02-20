6bf36eba4ed81965a295b11a60a6b0791c2f890e
romainlavabre <romainlavabre98@gmail.com>
2025-02-20 14:44:23 +0100

    [1.0.0] ADD fairfair-cloud/support#148

--- collector/run-live.sh
+++ collector/run-live.sh
@@ -6,6 +6,7 @@ if [ ! -d "/var/docker-volume/home-automation-transmitter/zigbee2mqtt" ]; then
     cp configuration.yaml /var/docker-volume/home-automation-transmitter/zigbee2mqtt/configuration.yaml
 fi
 
+sudo chown -Rf $USER /var/docker-volume/home-automation-transmitter/zigbee2mqtt
 rm -Rf /var/docker-volume/home-automation-transmitter/zigbee2mqtt/external_converters
 cp -r external_converters /var/docker-volume/home-automation-transmitter/zigbee2mqtt
 
