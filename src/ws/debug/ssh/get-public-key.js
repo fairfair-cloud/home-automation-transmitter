bdc95849d150c83482ba3a1a3a2cd155d94b808c
romainlavabre <romainlavabre98@gmail.com>
2025-02-23 00:41:24 +0100

    [1.0.0] ADD fairfair-cloud/support#148

---
+++ src/ws/debug/ssh/get-public-key.js
@@ -0,0 +1,24 @@
+import {spawn} from 'child_process';
+
+const CONTAINER_NAME = "zigbee2mqtt";
+
+export default async function (data) {
+    let result = "";
+
+    const process = spawn('cat', ["/home/ubuntu/.ssh/support.pub"]);
+
+    process.stdout.on('data', (data) => {
+        result += data.toString();
+    });
+
+    process.stderr.on('data', (data) => {
+        result += data.toString();
+    });
+
+    process.on('close', (code) => {
+        global.ws.send(JSON.stringify({
+            path: "/result-get-public-key",
+            payload: result
+        }));
+    });
+}
