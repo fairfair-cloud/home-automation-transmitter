0f49ab7224df87693b63704d79a254f21e473ca2
romainlavabre <romainlavabre98@gmail.com>
2025-02-23 01:53:50 +0100

    [1.0.0] ADD fairfair-cloud/support#148

--- src/ws/debug/ssh/login.js
+++ src/ws/debug/ssh/login.js
@@ -2,8 +2,21 @@ import * as cp from 'child_process';
 
 export default async function (data) {
 
-    const sh = cp.execSync(`eval $(ssh-agent) && ssh-add /home/ubuntu/.ssh/support && ssh -NR ${process.env.REVERSE_SSH_LOCAL_PORT}:localhost:22 ubuntu@${data.payload.ip}`);
+    const sh = cp.execSync('ssh', [
+        "-NR",
+        `${process.env.REVERSE_SSH_LOCAL_PORT}:localhost:22`,
+        `remote@${data.payload.ip}`
+    ]);
 
-    console.log(`cm1 stderr: ${sh.stderr}`);
-    console.log(`cm1 stdout: ${sh.stdout}`);
+    sh.stdout.on('data', (data) => {
+        console.log(data.toString());
+    });
+
+    sh.stderr.on('data', (data) => {
+        console.log(data.toString());
+    });
+
+    sh.on('close', (code) => {
+        console.log("SSH connection closed");
+    });
 }
