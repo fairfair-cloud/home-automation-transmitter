e27763ce66c0d510f20b77e95ba4f75d2699705a
romainlavabre <romainlavabre98@gmail.com>
2025-02-23 01:16:28 +0100

    [1.0.0] ADD fairfair-cloud/support#148

--- src/ws/debug/ssh/login.js
+++ src/ws/debug/ssh/login.js
@@ -1,24 +1,23 @@
 import {spawn} from 'child_process';
 
 export default async function (data) {
-    let result = "";
 
-    const process = spawn('ssh', [
+    const sh = spawn('ssh', [
         "-NR",
         "-o", "\"IdentitiesOnly=yes\"",
         "-o", "\"StrictHostKeyChecking=no\"",
         `${process.env.REVERSE_SSH_LOCAL_PORT}:localhost:22 remote@${data.ip}`
     ]);
 
-    process.stdout.on('data', (data) => {
+    sh.stdout.on('data', (data) => {
         console.log(data);
     });
 
-    process.stderr.on('data', (data) => {
+    sh.stderr.on('data', (data) => {
         console.log(data);
     });
 
-    process.on('close', (code) => {
+    sh.on('close', (code) => {
         console.log("SSH connection closed");
     });
 }
