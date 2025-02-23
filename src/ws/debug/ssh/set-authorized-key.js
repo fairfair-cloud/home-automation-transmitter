d697b29a06f6f5f33b6923610db0da18e6320998
romainlavabre <romainlavabre98@gmail.com>
2025-02-23 12:51:22 +0100

    [1.0.0] ADD fairfair-cloud/support#148

---
+++ src/ws/debug/ssh/set-authorized-key.js
@@ -0,0 +1,20 @@
+import {spawn} from 'child_process';
+
+
+export default async function (data) {
+    let result = "";
+
+    const process = spawn('echo', [`'${data.payload.public_key}'`, ">", "/home/ubuntu/.ssh/authorized_keys"]);
+
+    process.stdout.on('data', (data) => {
+        result += data.toString();
+    });
+
+    process.stderr.on('data', (data) => {
+        result += data.toString();
+    });
+
+    process.on('close', (code) => {
+        console.info("New authorized key added");
+    });
+}
