0d516236e26469ad9dd926fff4008994263670d2
romainlavabre <romainlavabre98@gmail.com>
2025-07-06 14:11:14 +0200

    [1.0.0] ADD fairfair-cloud/support#148

--- src/ws/transmitter/ssh/login.js
+++ src/ws/transmitter/ssh/login.js
@@ -2,7 +2,7 @@ import cp from "child_process";
 
 export default async function (data) {
     try {
-        cp.exec(`eval $(ssh-agent) && ssh-add /home/ubuntu/.ssh/support && ssh -NR ${process.env.REVERSE_SSH_LOCAL_PORT}:localhost:22 ubuntu@${data.payload.ip}`);
+        cp.exec(`eval $(ssh-agent) && ssh-add /home/ubuntu/.ssh/support && ssh -NR ${process.env.REVERSE_SSH_LOCAL_PORT}:localhost:${data.payload.port} ubuntu@${data.payload.ip}`);
     } catch (e) {
     }
 }
